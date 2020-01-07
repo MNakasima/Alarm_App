4a6b34af796ebef1ff74f33b06ede67a7078de80
Maruko <marco.k95@hotmail.com>
2020-01-07 16:10:10 -0300

    basic layout

--- app/src/main/java/com/mnakasima/alarmapp/MainActivity.kt
+++ app/src/main/java/com/mnakasima/alarmapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mnakasima.alarmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun buSetTime(view: View){}
+
 }
