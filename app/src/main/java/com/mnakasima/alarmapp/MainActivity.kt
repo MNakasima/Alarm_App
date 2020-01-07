6b5b03e5a96764d5d20aa33deab6cf60ac372489
Maruko <marco.k95@hotmail.com>
2020-01-07 16:38:14 -0300

    simple functionality of alarm app using fragments

--- app/src/main/java/com/mnakasima/alarmapp/MainActivity.kt
+++ app/src/main/java/com/mnakasima/alarmapp/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.mnakasima.alarmapp
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.View
-
+import androidx.appcompat.app.AppCompatActivity
+import com.mnakasima.alarmapp.R
+import  kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val saveData= SaveData(applicationContext)
+
+        tvShowTime.text= saveData.getHour().toString() + ":" + saveData.getMinute().toString()
     }
 
-    fun buSetTime(view: View){}
 
+    fun BuSetTime(view:View){
+        val popTime= PopTime()
+        val fm=supportFragmentManager
+        popTime.show(fm,"Select time")
+
+
+    }
+
+    fun SetTime(Hours:Int,Minute:Int){
+
+        tvShowTime.text= "$Hours:$Minute"
+
+        val saveData=SaveData(applicationContext)
+        saveData.SaveData(Hours,Minute)
+        saveData.setAlarm()
+
+
+    }
 }
